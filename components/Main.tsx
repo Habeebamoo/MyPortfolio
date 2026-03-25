961c4e5d35646143ff28e105f224733feb88f369
Habeebamoo <habeebamoo08@gmail.com>
2026-03-25 14:33:32 +0100

    updated picture size

--- components/Main.tsx
+++ components/Main.tsx
@@ -25,7 +25,7 @@ const Main = () => {
       </section>
 
       <div className="flex-center mt-20">
-        <img src="/picture.jpg" className="h-70 md:h-80" />
+        <img src="/picture.jpg" className="h-60 md:h-80" />
       </div>
 
       {/* Projects */}
